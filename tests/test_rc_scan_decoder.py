03a9512ea40b092843f82ce474596ecdf973e956
grigory <grigory.timofeev@gmail.com>
2019-10-23 06:19:52 +0700

    Extend tests

--- tests/test_rc_scan_decoder.py
+++ tests/test_rc_scan_decoder.py
@@ -3,10 +3,10 @@ from unittest import TestCase
 import numpy as np
 
 from polar_codes.decoders import RCSCANDecoder
-from polar_codes.decoders.rc_scan_decoder import RCSCANNode
+from polar_codes.decoders.rc_scan_decoder import INFINITY
 
 
-class TestFastSSCDecoder(TestCase):
+class TestRCSCANDecoder(TestCase):
     @classmethod
     def setUpClass(cls):
         cls.received_llr = np.array([
@@ -24,38 +24,43 @@ class TestFastSSCDecoder(TestCase):
     def test_zero_node_decoder(self):
         mask = np.zeros(self.length, dtype=np.int8)
         decoder = RCSCANDecoder(mask=mask, is_systematic=True)
-        self.assertEqual(len(decoder._decoding_tree.leaves), 1)
-
         decoder.initialize(self.received_llr)
         decoder()
+
+        self.assertEqual(len(decoder._decoding_tree.leaves), 1)
         np.testing.assert_equal(
             decoder.result,
-            np.ones(self.length, dtype=np.double) * RCSCANNode.INFINITY
+            np.ones(self.length, dtype=np.double) * INFINITY
         )
 
     def test_one_node_decoder(self):
         mask = np.ones(self.length, dtype=np.int8)
         decoder = RCSCANDecoder(mask=mask, is_systematic=True)
-        self.assertEqual(len(decoder._decoding_tree.leaves), 1)
-
         decoder.initialize(self.received_llr)
         decoder()
-        np.testing.assert_equal(
-            decoder.result,
-            np.zeros(self.length)
-        )
+
+        self.assertEqual(len(decoder._decoding_tree.leaves), 1)
+        np.testing.assert_equal(decoder.result, np.zeros(self.length))
 
     def test_zero_one_decoder(self):
+        expected = np.array([
+            0.0506,
+            -0.0552,
+            -1.5304,
+            -2.1233,
+            -2.7273,
+            -8.7327,
+            0.1087,
+            1.6463,
+        ])
+
         mask = np.append(
             np.zeros(self.length // 2, dtype=np.int8),
             np.ones(self.length // 2, dtype=np.int8)
         )
         decoder = RCSCANDecoder(mask=mask, is_systematic=True)
-        self.assertEqual(len(decoder._decoding_tree.leaves), 2)
-
         decoder.initialize(self.received_llr)
         decoder()
-        np.testing.assert_equal(
-            decoder.result,
-            np.zeros(self.length)
-        )
+
+        self.assertEqual(len(decoder._decoding_tree.leaves), 2)
+        np.testing.assert_equal(decoder.result, expected)
--- tests/test_rc_scan_node.py
+++ tests/test_rc_scan_node.py
@@ -2,10 +2,10 @@ from unittest import TestCase
 
 import numpy as np
 
-from polar_codes.decoders.rc_scan_decoder import RCSCANNode
+from polar_codes.decoders.rc_scan_decoder import INFINITY, RCSCANNode
 
 
-class RCSCANNodeTest(TestCase):
+class TestRCSCANNode(TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -19,7 +19,7 @@ class RCSCANNodeTest(TestCase):
         node.compute_leaf_beta()
         np.testing.assert_equal(
             node.beta,
-            np.ones(4) * RCSCANNode.INFINITY,
+            np.ones(4) * INFINITY,
         )
 
     def test_one_node(self):
@@ -34,16 +34,26 @@ class RCSCANNodeTest(TestCase):
         )
 
     def test_with_multiple_nodes(self):
-        node = RCSCANNode(mask=np.array([0, 1, 0, 0, 0, 0, 1, 1, ]))
+        node = RCSCANNode(mask=np.array([
+            0,
+            1,
+            0, 0,
+            0, 0,
+            1, 1,
+            0, 0, 0, 0,
+            1, 1, 1, 1,
+        ]))
         self.assertEqual(node._node_type, RCSCANNode.OTHER)
 
-        leaf_path_lengths = [4, 4, 3, 3, 3]
+        leaf_path_lengths = [5, 5, 4, 4, 4, 3, 3]
         leaf_masks = [
             np.array([0, ]), np.array([1, ]), np.array([0, 0, ]),
             np.array([0, 0, ]), np.array([1, 1, ]),
+            np.array([0, 0, 0, 0, ]), np.array([1, 1, 1, 1, ]),
         ]
         leaf_types = [
             node.ZERO_NODE, node.ONE_NODE, node.ZERO_NODE,
+            node.ZERO_NODE, node.ONE_NODE,
             node.ZERO_NODE, node.ONE_NODE,
         ]
 
